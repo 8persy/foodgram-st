6507d7dccd10bb3819f188aba32d021ddbb63339
8percy8 <izergin.v05@mail.ru>
2025-05-29 18:03:31 +0300

    feat: custom pagination

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -146,7 +146,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
