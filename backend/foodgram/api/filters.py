fed434c4f7ae9004ee4427effa66af09f4b21571
8percy8 <izergin.v05@mail.ru>
2025-06-13 15:14:02 +0300

    fix: something

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -15,12 +15,12 @@ class RecipeFilter(FilterSet):
         model = Recipe
         fields = ('author', 'is_favorited', 'is_in_shopping_cart')
 
-    def get_is_favorited(self, queryset, name, value):
+    def get_is_favorited(self, queryset, value):
         if self.request.user.is_authenticated and value:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
+    def get_is_in_shopping_cart(self, queryset, value):
         if self.request.user.is_authenticated and value:
             return queryset.filter(carts__user=self.request.user)
         return queryset
