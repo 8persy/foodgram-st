0fb54866b6cdddf55008c358f91afa4793add6c9
8percy8 <izergin.v05@mail.ru>
2025-06-15 09:20:54 +0300

    fix: shopping cart filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -4,23 +4,19 @@ from recipes.models import Recipe, Ingredient
 
 
 class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter(
-        method='get_is_favorited'
-    )
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart'
-    )
+    is_favorited = filters.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('author', 'is_favorited', 'is_in_shopping_cart')
 
-    def get_is_favorited(self, queryset, value):
+    def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def get_is_in_shopping_cart(self, queryset, value):
+    def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
             return queryset.filter(carts__user=self.request.user)
         return queryset
